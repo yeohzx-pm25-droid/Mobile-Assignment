70d7842cb7b5da37fc6e544f40a80fdbeb03242a
Chris Yeoh <yeohzx-pm25@student.tarc.edu.my>
2026-08-24 10:52:15 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/dcsg1_mobileassignment/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.dcsg1_mobileassignment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dcsg1_mobileassignment.ui.theme.DCSG1_MobileAssignmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DCSG1_MobileAssignmentTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DCSG1_MobileAssignmentTheme {
+        Greeting("Android")
+    }
+}
